13d4a7ad51d64917368dc905837c518486ed8569
akhilsai0099 <akhilsai0099@gmail.com>
2024-04-21 20:06:43 +0530

    netlify

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,13 +1,16 @@
 // YOUR_BASE_DIRECTORY/netlify/functions/api.ts
-
+const cors = require("cors");
 import express, { Router } from "express";
 import serverless from "serverless-http";
-
+const rootRouter = require("../../routes");
+require("dotenv").config();
 const api = express();
+api.use(cors());
+api.use(express.json());
 
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use("/api/", router);
+api.use("/api/v1", rootRouter);
 
 export const handler = serverless(api);
